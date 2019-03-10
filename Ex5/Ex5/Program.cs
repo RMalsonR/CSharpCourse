2372d459ad3524edf16b82033fe5ac506bb2b3b7
Renat <43974203+RMalsonR@users.noreply.github.com>
2019-03-11 00:11:45 +0300

    Comment Program.cs
    В целом - молодец. Есть недочёты, которые нужно исправить. Не забывай тестировать программу.

--- Ex5/Ex5/Program.cs
+++ Ex5/Ex5/Program.cs
@@ -12,19 +12,19 @@ namespace Ex5
         static void Main(string[] args)
         {
             Encoding encoding = Encoding.GetEncoding(1251);
-            StreamReader streamReader = new StreamReader("Input.txt", encoding);
+            StreamReader streamReader = new StreamReader("Input.txt", encoding); //Скорее всего компилятор будет ругаться на то, что не нашел файл
 
-            string text = streamReader.ReadLine();
-
+            string text = streamReader.ReadLine(); // Тут ты считываешь только одну строку
+            
             streamReader.Close();
 
             string shiftAsText = Console.ReadLine();
-            int shift = Convert.ToInt32(shiftAsText);
+            int shift = Convert.ToInt32(shiftAsText); // Int32 всегда можно заменить на int при простейших операциях
             string result = string.Empty;
 
-            for (int i = 0; i < text.Length; i++)
+            for (int i = 0; i < text.Length; i++) // А как будет шифроваться буква 'Я'? По идее буквой 'В' при сдвиге 3 шага
             {
-                result += (char)(text[i] + shift);
+                result += (char)(text[i] + shift); // Но тут не учтено условие (char)(text[i]+shift) > 'Я'. Погугли формулы, решение простое
             }
 
             StreamWriter streamWriter = new StreamWriter("Output.txt", false, encoding);
